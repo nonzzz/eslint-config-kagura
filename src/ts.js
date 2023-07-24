78ee16caf0758509a5a823cf51be2c2ca7d6ddb9
unknown <812137533@qq.com>
2023-07-24 10:25:33 +0800

    feat: add import plugin

--- src/ts.js
+++ src/ts.js
@@ -7,9 +7,19 @@ module.exports = {
       files: ['*.ts', '*.tsx', '*.mts', '*.cts'],
       parser: '@typescript-eslint/parser',
       plugins: ['@typescript-eslint'],
-      extends: ['plugin:@typescript-eslint/recommended'],
+      extends: ['plugin:import/typescript', 'plugin:@typescript-eslint/recommended'],
+      settings: {
+        'import/resolver': {
+          node: { extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx', '.d.ts'] }
+        }
+      },
       rules: {
-        '@typescript-eslint/no-explicit-any': 0
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/space-infix-ops': 'error',
+        '@typescript-eslint/type-annotation-spacing': ['error', {
+          before: false,
+          after: true
+        }]
       }
     }
   ]
