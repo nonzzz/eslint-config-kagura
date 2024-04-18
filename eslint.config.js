57f39f7ea0996588282dc4e42c04a621846cae1a
kanno <812137533@qq.com>
2024-04-18 11:00:28 +0800

    chore: rlease v2.2.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,3 @@
 const { nonzzz } = require('./dist')
 
-module.exports = nonzzz({ ts: true })
+module.exports = nonzzz({ ts: true, jsx: true, react: true })
--- src/conf/react.ts
+++ src/conf/react.ts
@@ -2,14 +2,14 @@ import { FlatConfig } from 'src/interface'
 import { GLOB_JSX, GLOB_TSX } from '~/constants'
 import { pluginJsxA11y, pluginReact, pluginReactHooks } from '~/plugins'
 
-// https://github.com/SukkaW/eslint-config-sukka/blob/master/packages/react/src/index.ts
+// https://github.com/SukkaW/eslint-config-sukka/blob/master/packages/@eslint-react/src/index.ts
 
 export function react(): FlatConfig[] {
   return [
     {
       files: [GLOB_TSX, GLOB_JSX],
       plugins: {
-        react: pluginReact,
+        '@eslint-react': pluginReact as any,
         'jsx-a11y': pluginJsxA11y,
         'react-hooks': pluginReactHooks
       },
@@ -19,66 +19,82 @@ export function react(): FlatConfig[] {
         }
       },
       rules: {
-        ...pluginReact.configs.recommended.rules,
-        ...pluginReact.configs['jsx-runtime'].rules,
         ...pluginReactHooks.configs.recommended.rules,
         ...pluginJsxA11y.configs.recommended.rules,
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-filename-extension': ['warn', {
-          extensions: ['.jsx', '.tsx']
-        }],
-        'react/prop-types': 'off',
-        'react/display-name': ['off', { ignoreTranspilerName: false }],
-        'react/no-unknown-property': ['error', { ignore: ['css', 'jsx'] }],
-        'react/jsx-boolean-value': ['error', 'never', { always: [] }],
-        'react/jsx-handler-names': ['off', {
-          eventHandlerPrefix: 'handle',
-          eventHandlerPropPrefix: 'on'
-        }],
-        'react/jsx-no-bind': ['error', {
-          ignoreRefs: true,
-          allowArrowFunctions: true,
-          allowFunctions: false,
-          allowBind: false,
-          ignoreDOMComponents: true
-        }],
-        'react/jsx-pascal-case': ['error', {
-          allowAllCaps: true,
-          ignore: []
-        }],
-        'react/prefer-stateless-function': ['error', { ignorePureComponents: true }],
-        'react/require-render-return': 'error',
-        'react/self-closing-comp': 'error',
-        'react/sort-comp': ['error', {
-          order: [
-            'static-variables',
-            'static-methods',
-            'instance-variables',
-            'lifecycle',
-            '/^handle.+$/',
-            '/^on.+$/',
-            'getters',
-            'setters',
-            '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
-            'instance-methods',
-            'everything-else',
-            'rendering'
-          ],
-          groups: {
-            lifecycle: [
-              'displayName',
-              'propTypes',
-              'contextTypes',
-              'childContextTypes',
-              'mixins',
-              'statics',
-              'defaultProps',
-              'constructor',
-              'getDefaultProps',
+        ...pluginReact.configs.recommended.rules,
+        '@eslint-react/no-string-refs': 'error',
+        '@eslint-react/no-comment-textnodes': 'error',
+        '@eslint-react/dom/no-unsafe-target-blank': 'error',
+        '@eslint-react/no-children-prop': 'error',
+        '@eslint-react/dom/no-dangerously-set-innerhtml-with-children': 'error',
+        '@eslint-react/dom/no-render-return-value': 'error',
+        '@eslint-react/no-direct-mutation-state': 'error',
+        '@eslint-react/dom/no-find-dom-node': 'error',
+        '@eslint-react/no-unsafe-component-will-mount': 'warn',
+        '@eslint-react/no-unsafe-component-will-receive-props': 'warn',
+        '@eslint-react/no-unsafe-component-will-update': 'warn',
+        '@eslint-react/no-unused-state': 'error',
+        '@eslint-react/no-access-state-in-setstate': 'error',
+        '@eslint-react/dom/no-script-url': 'error',
+        '@eslint-react/no-unused-class-component-members': 'error',
+        '@eslint-react/naming-convention/filename': [
+          'error',
+          { rule: 'kebab-case' }
+        ],
+        '@eslint-react/naming-convention/filename-extension': [
+          'error',
+          { allow: 'as-needed' }
+        ],
+        'react-prefer-function-component/react-prefer-function-component':
+          'error',
+        '@eslint-react/naming-convention/use-state': 'error',
+        '@eslint-react/prefer-shorthand-boolean': 'error',
+        '@eslint-react/no-missing-key': 'error',
+        '@eslint-react/no-array-index-key': 'error',
+        '@eslint-react/no-duplicate-key': 'error',
+        '@eslint-react/no-implicit-key': 'error',
+        '@eslint-react/no-useless-fragment': 'error',
+
+        // Prevent react contexts from taking non-stable values
+        '@eslint-react/no-unstable-context-value': 'error',
+
+        // Prevent creating uns dable components inside components
+        // https://github.com/jsx-eslint/eslint-plugin-react/blob/c2a790a3472eea0f6de984bdc3ee2a62197417fb/docs/rules/no-unstable-nested-components.md
+        // 'react/no-unstable-nested-components': 'off',
+        '@eslint-react/no-nested-components': 'error',
+
+        // Enforce sandbox attribute on iframe elements
+        // https://github.com/jsx-eslint/eslint-plugin-react/blob/c8833f301314dab3e79ef7ac4cf863e4d5fa0019/docs/rules/iframe-missing-sandbox.md
+        // 'react/iframe-missing-sandbox': 'off',
+        '@eslint-react/dom/no-missing-iframe-sandbox': 'warn',
+        '@eslint-react/dom/no-unsafe-iframe-sandbox': 'warn',
+
+        // Prevent problematic leaked values from being rendered
+        // https://github.com/jsx-eslint/eslint-plugin-react/blob/c42b624d0fb9ad647583a775ab9751091eec066f/docs/rules/jsx-no-leaked-render.md
+        // 'react/jsx-no-leaked-render': 'off',
+        '@eslint-react/no-leaked-conditional-rendering': 'error',
+
+        // To prevent potential unnecessary rerenders, and performance regressions
+        // https://github.com/jsx-eslint/eslint-plugin-react/blob/66b58dd4864678eb869a7bf434c72ff7ac530eb1/docs/rules/no-object-type-as-default-prop.md
+        // 'react/no-object-type-as-default-prop': 'off',
+        '@eslint-react/no-unstable-default-props': 'error',
+
+        // default type is "submit" which refresh the page
+        // 'react/button-has-type': 'off',
+        '@eslint-react/dom/no-missing-button-type': 'error',
+
+        // <svg:rect> react does not support
+        // 'react/no-namespace': 'off',
+        '@eslint-react/dom/no-namespace': 'error',
+
+        'class-methods-use-this': [
+          'error',
+          {
+            exceptMethods: [
+              'render',
               'getInitialState',
-              'state',
+              'getDefaultProps',
               'getChildContext',
-              'getDerivedStateFromProps',
               'componentWillMount',
               'UNSAFE_componentWillMount',
               'componentDidMount',
@@ -87,45 +103,26 @@ export function react(): FlatConfig[] {
               'shouldComponentUpdate',
               'componentWillUpdate',
               'UNSAFE_componentWillUpdate',
-              'getSnapshotBeforeUpdate',
               'componentDidUpdate',
+              'componentWillUnmount',
               'componentDidCatch',
-              'componentWillUnmount'
-            ],
-            rendering: [
-              '/^render.+$/',
-              'render'
+              'getSnapshotBeforeUpdate'
             ]
           }
-        }
         ],
-        'react/no-array-index-key': 'off',
-        'react/void-dom-elements-no-children': 'error',
-        'react/no-unused-state': 'error',
-        'react/no-access-state-in-setstate': 'error',
-        'react/no-this-in-sfc': 'error',
-        'react/jsx-no-script-url': ['error', [
+
+        'jsx-a11y-minimal/alt-text': [
+          'warn',
           {
-            name: 'Link',
-            props: ['to', 'href']
-          },
-          {
-            name: 'NextLink',
-            props: ['href']
+            elements: ['img'],
+            img: ['Image']
           }
-        ]],
-        'react/jsx-key': ['warn', { checkFragmentShorthand: true, checkKeyMustBeforeSpread: true, warnOnDuplicates: true }],
-        'react/jsx-uses-vars': 'error',
-        'react/jsx-no-useless-fragment': 'error',
-        'react/jsx-no-constructed-context-values': 'error',
-        'react/no-unstable-nested-components': 'error',
-        'react/no-unused-class-component-methods': 'error',
-        'react/hook-use-state': 'off',
-        'react/iframe-missing-sandbox': 'warn',
-        'react/jsx-no-leaked-render': 'off',
-        'react/no-object-type-as-default-prop': 'warn',
-        'react/button-has-type': 'error',
-        'react/no-namespace': 'error' 
+        ],
+        'jsx-a11y-minimal/aria-props': 'warn',
+        'jsx-a11y-minimal/aria-proptypes': 'warn',
+        'jsx-a11y-minimal/aria-unsupported-elements': 'warn',
+        'jsx-a11y-minimal/role-has-required-aria-props': 'warn',
+        'jsx-a11y-minimal/role-supports-aria-props': 'warn'
       }
     }
   ]
